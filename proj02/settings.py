b4e1e2d72a4b062fc659e4d8a96581764f374051
Amy0322 <45816739+Amy0322@users.noreply.github.com>
2020-10-18 00:28:25 +0800

    Update settings.py

--- proj02/settings.py
+++ proj02/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'proj02.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',  # PostgreSQL
+        'NAME': 'muxing',  # 資料庫名稱
+        'USER': 'postgres',  # 資料庫帳號
+        'PASSWORD': '987654321',  # 資料庫密碼
+        'HOST': 'localhost',  # Server(伺服器)位址
+        'PORT': '5432'  # PostgreSQL Port號
     }
 }
 
